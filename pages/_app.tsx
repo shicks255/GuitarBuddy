144f6a99fbd428762253edff215b08819f865267
shicks <shicks255@yahoo.com>
2023-01-19 16:02:16 -0600

    analytics test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -39,26 +39,26 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/* <script
+        <script
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-5MRWHRJ');`,
-        }}
-      /> */}
+          }}
+        />
         <title>Guitar-Pal</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.webmanifest" />
       </Head>
-      <Script id="google-tag-manager" strategy="afterInteractive">
+      {/* <Script id="google-tag-manager" strategy="afterInteractive">
         {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-5MRWHRJ');`}
-      </Script>
+      </Script> */}
       <noscript>
         <iframe
           src="https://www.googletagmanager.com/ns.html?id=GTM-5MRWHRJ"

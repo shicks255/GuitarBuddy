74baf28563bc93293168d6f86dcd241229805fdc
shicks <shicks255@yahoo.com>
2023-01-19 16:30:26 -0600

    another try

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -48,17 +48,15 @@ export default function MyApp({ Component, pageProps }) {
           })(window,document,'script','dataLayer','GTM-5MRWHRJ');`,
           }}
         /> */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5MRWHRJ');`,
+          }}
+        ></script>
         <title>Guitar-Pal</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.webmanifest" />
       </Head>
-      <Script
-        id="google-tag-manager"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5MRWHRJ');`,
-        }}
-      ></Script>
       <body>
         <noscript
           dangerouslySetInnerHTML={{

3b5b06ad51cf0de3a9178e13f8d7706ad2201df0
shicks <shicks255@yahoo.com>
2023-02-18 20:02:41 -0600

    some refactorings

--- pages/rhythms.tsx
+++ pages/rhythms.tsx
@@ -4,6 +4,11 @@ import * as Tone from 'tone';
 import { Sampler } from 'tone/build/esm/instrument';
 import { patterns } from '../patterns';
 import { pattern as keys } from '../utils';
+import PlayingIcon from '../components/icons/PlayingIcon';
+import StopIcon from '../components/icons/StopIcon';
+import ClearIcon from '../components/icons/ClearIcon';
+import SpeakerIcon from '../components/icons/SpeakerIcon';
+import WrenchIcon from '../components/icons/WrenchIcon';
 
 const Bass = async (
   note: string,
@@ -16,172 +21,40 @@ const Bass = async (
   }
 };
 
-const Kick = async (
-  ctx: AudioContext,
-  time: number,
-  vol: number,
-  sample: Sampler
-) => {
-  if (true) {
-    if (vol > -10) {
-      if (time > 0) {
-        sample.triggerAttackRelease(['A1'], 0.3, time);
-      } else {
-        sample.triggerAttackRelease(['A1'], 0.3);
-      }
+const Kick = async (time: number, vol: number, sample: Sampler) => {
+  if (vol > -10) {
+    if (time > 0) {
+      sample.triggerAttackRelease(['A1'], 0.3, time);
+    } else {
+      sample.triggerAttackRelease(['A1'], 0.3);
     }
-  } else {
-    const osc = ctx.createOscillator();
-    osc.type = 'sine';
-
-    const gain = ctx.createGain();
-    const distortion = ctx.createWaveShaper();
-
-    const k = 1 / 4;
-    const samples = 44100;
-    const curve = new Float32Array(samples);
-    const deg = Math.PI / 180;
-    let i = 0;
-    let x;
-
-    for (; i < samples; ++i) {
-      x = (i * 2) / samples - 1;
-      curve[i] = ((3 + k) * x * 20 * deg) / (Math.PI + k * Math.abs(x));
-    }
-
-    distortion.curve = curve;
-
-    osc.connect(gain);
-    gain.connect(distortion);
-    distortion.connect(ctx.destination);
-
-    osc.frequency.setValueAtTime(167.1, time + 0.001);
-    gain.gain.linearRampToValueAtTime(vol, time + 0.1);
-
-    osc.frequency.exponentialRampToValueAtTime(1, time + 0.5);
-    gain.gain.exponentialRampToValueAtTime(0.01 * vol, time + 0.5);
-    gain.gain.linearRampToValueAtTime(0, time + 0.5);
-
-    osc.start(time);
-    osc.stop(time + 0.5 + 0.1);
   }
 };
 
-const Snare = async (
-  ctx: AudioContext,
-  time: number,
-  vol: number,
-  sample: Sampler
-) => {
-  if (true) {
-    if (vol > -10) {
-      if (time > 0) {
-        sample.triggerAttackRelease(['B1'], 0.4, time);
-      } else {
-        sample.triggerAttackRelease(['B1'], 0.4);
-      }
+const Snare = async (time: number, vol: number, sample: Sampler) => {
+  if (vol > -10) {
+    if (time > 0) {
+      sample.triggerAttackRelease(['B1'], 0.4, time);
+    } else {
+      sample.triggerAttackRelease(['B1'], 0.4);
     }
-  } else {
-    const noise = ctx.createBufferSource();
-
-    const bufferSize = ctx.sampleRate;
-    const buffer = ctx.createBuffer(1, bufferSize, ctx.sampleRate);
-    const output = buffer.getChannelData(0);
-
-    for (let i = 0; i < bufferSize; i++) {
-      output[i] = Math.random() * 2 - 1;
-    }
-
-    noise.buffer = buffer;
-
-    const noiseFilter = ctx.createBiquadFilter();
-    noiseFilter.type = 'highpass';
-    noiseFilter.frequency.value = 1000;
-    noise.connect(noiseFilter);
-
-    const noiseEnvelope = ctx.createGain();
-    noiseFilter.connect(noiseEnvelope);
-
-    noiseEnvelope.connect(ctx.destination);
-
-    const osc = ctx.createOscillator();
-    osc.type = 'triangle';
-
-    const oscEnvelope = ctx.createGain();
-    osc.connect(oscEnvelope);
-    oscEnvelope.connect(ctx.destination);
-
-    noiseEnvelope.gain.setValueAtTime(vol, time);
-    noiseEnvelope.gain.exponentialRampToValueAtTime(0.01, time + 0.2);
-    noise.start(time);
-
-    osc.frequency.setValueAtTime(100, time);
-    oscEnvelope.gain.setValueAtTime(0.7 * vol, time);
-    oscEnvelope.gain.exponentialRampToValueAtTime(0.01 * vol, time + 0.7 / 2);
-    osc.start(time);
-
-    osc.stop(time + 0.7);
-    noise.stop(time + 0.7);
   }
 };
 
-const HiHat = async (
-  ctx: AudioContext,
-  time: number,
-  vol: number,
-  sample: Sampler
-) => {
+const HiHat = async (time: number, vol: number, sample: Sampler) => {
   if (vol === 0) {
     return;
   }
-  if (true) {
-    if (vol > -10) {
-      if (time > 0) {
-        sample.triggerAttackRelease(['C1'], 0.3, time);
-      } else {
-        sample.triggerAttackRelease(['C1'], 0.3);
-      }
+  if (vol > -10) {
+    if (time > 0) {
+      sample.triggerAttackRelease(['C1'], 0.3, time);
+    } else {
+      sample.triggerAttackRelease(['C1'], 0.3);
     }
-  } else {
-    const oscEnvelope = ctx.createGain();
-    const bndPass = ctx.createBiquadFilter();
-    bndPass.type = 'bandpass';
-    bndPass.frequency.value = 20000;
-    bndPass.Q.value = 0.2;
-    const hiPass = ctx.createBiquadFilter();
-    hiPass.type = 'highpass';
-    hiPass.frequency.value = 5000;
-    const panner = ctx.createStereoPanner();
-
-    bndPass.connect(hiPass);
-    hiPass.connect(oscEnvelope);
-    oscEnvelope.connect(panner);
-    panner.connect(ctx.destination);
-
-    panner.pan.value = Math.cos(time * 4);
-    const tone = 130.81;
-    const ratios = [1, 1.342, 1.2312, 1.6532, 1.9523, 2.1523];
-    ratios.forEach((rat) => {
-      const osc = ctx.createOscillator();
-      osc.type = 'square';
-      osc.frequency.value = tone * rat;
-      osc.connect(bndPass);
-      osc.start(time);
-      osc.stop(time + 0.5);
-    });
-    oscEnvelope.gain.setValueAtTime(0.00001 * vol, time);
-    oscEnvelope.gain.exponentialRampToValueAtTime(1 * vol, time + 0.067 * 0.9);
-    oscEnvelope.gain.exponentialRampToValueAtTime(0.3 * vol, time + 0.1 * 0.9);
-    oscEnvelope.gain.exponentialRampToValueAtTime(0.00001 * vol, time + 0.9);
   }
 };
 
-const Ride = async (
-  ctx: AudioContext,
-  time: number,
-  vol: number,
-  sample: Sampler
-) => {
+const Ride = async (time: number, vol: number, sample: Sampler) => {
   if (vol < -10) {
     return;
   }
@@ -250,17 +123,6 @@ const Cymbal = async (ctx: AudioContext, time: number, vol: number) => {
     time + decay + fxAmount / 50
   );
 };
-
-// const Bass = (
-//   ctx: AudioContext,
-//   time: number,
-//   vol: number,
-//   note: { note: string; duration: string }
-// ) => {
-//   const s = new Tone.Synth().toDestination();
-//   s.triggerAttackRelease(note.note, Tone.Time(note.duration).toSeconds(), time);
-//   //   s.oscillator.type = 'sawtooth';
-// };
 
 const calculateBass = (key: string, cents: number) => {
   let octave = '3';
@@ -641,16 +503,16 @@ const Rythms = () => {
               Cymbal(ctx.current, time, cymbalVol);
             }
             if (i === 1) {
-              Ride(ctx.current, time, rideVol, rideSample);
+              Ride(time, rideVol, rideSample);
             }
             if (i === 2) {
-              HiHat(ctx.current, time, hatVol, hatSample);
+              HiHat(time, hatVol, hatSample);
             }
             if (i === 3) {
-              Snare(ctx.current, time, snareVol, snareSample);
+              Snare(time, snareVol, snareSample);
             }
             if (i === 4) {
-              Kick(ctx.current, time, kickVol, kickSample);
+              Kick(time, kickVol, kickSample);
             }
           }
         });
@@ -659,18 +521,9 @@ const Rythms = () => {
       '16n'
     );
 
-    // const loop2 = new Tone.Loop((time) => {
-    //   steps[1].forEach((step, i) => {
-    //     if (step) {
-    //       Snare(ctx.current, time + i * Tone.Time('16n').toSeconds());
-    //     }
-    //   });
-    // }, '1m');
-
     if (isPlaying) {
       Tone.start().then(() => {
         loop.start(0);
-        // loop2.start(0);
         Tone.Transport.start();
       });
     } else {
@@ -679,7 +532,6 @@ const Rythms = () => {
 
     return () => {
       loop.dispose();
-      //   loop2.dispose();
     };
   }, [
     isPlaying,
@@ -733,36 +585,7 @@ const Rythms = () => {
           }`}
           onClick={() => start()}
         >
-          {isPlaying && (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          )}
-          {!isPlaying && (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
-              />
-            </svg>
-          )}
+          <PlayingIcon playing={isPlaying} />
         </button>
         <button
           className={`${
@@ -770,52 +593,10 @@ const Rythms = () => {
           }`}
           onClick={() => stop()}
         >
-          {!isPlaying && (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M4.5 7.5a3 3 0 013-3h9a3 3 0 013 3v9a3 3 0 01-3 3h-9a3 3 0 01-3-3v-9z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          )}
-          {isPlaying && (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M5.25 7.5A2.25 2.25 0 017.5 5.25h9a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25h-9a2.25 2.25 0 01-2.25-2.25v-9z"
-              />
-            </svg>
-          )}
+          <StopIcon isPlaying={isPlaying} />
         </button>
         <button onClick={() => clear()}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
+          <ClearIcon />
         </button>
         <div className="flex flex-col items-center">
           <span>Tempo</span>
@@ -834,39 +615,7 @@ const Rythms = () => {
         <div className="ml-4 flex flex-col items-center">
           <span>Bass Vol</span>
           <div>
-            {' '}
-            {bassVol === -40 && (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M17.25 9.75L19.5 12m0 0l2.25 2.25M19.5 12l2.25-2.25M19.5 12l-2.25 2.25m-10.5-6l4.72-4.72a.75.75 0 011.28.531V19.94a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.506-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.395C2.806 8.757 3.63 8.25 4.51 8.25H6.75z"
-                />
-              </svg>
-            )}
-            {bassVol > -40 && (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z"
-                />
-              </svg>
-            )}
+            <SpeakerIcon mute={bassVol === -40} />
           </div>
           <input
             type="range"
@@ -926,7 +675,7 @@ const Rythms = () => {
                   name="Ride"
                   vol={rideVol}
                   volControl={setRideVol}
-                  sampleFunction={() => Ride(ctx.current, 0, 1, rideSample)}
+                  sampleFunction={() => Ride(0, 1, rideSample)}
                 />
               ) : null}
               {rindx === 2 ? (
@@ -934,7 +683,7 @@ const Rythms = () => {
                   name="HiHat"
                   vol={hatVol}
                   volControl={setHatVol}
-                  sampleFunction={() => HiHat(ctx.current, 0, 1, hatSample)}
+                  sampleFunction={() => HiHat(0, 1, hatSample)}
                 />
               ) : null}
               {rindx === 3 ? (
@@ -942,7 +691,7 @@ const Rythms = () => {
                   name="Snare"
                   vol={snareVol}
                   volControl={setSnareVol}
-                  sampleFunction={() => Snare(ctx.current, 0, 1, snareSample)}
+                  sampleFunction={() => Snare(0, 1, snareSample)}
                 />
               ) : null}
               {rindx === 4 ? (
@@ -950,7 +699,7 @@ const Rythms = () => {
                   name="Kick"
                   vol={kickVol}
                   volControl={setKickVol}
-                  sampleFunction={() => Kick(ctx.current, 0, 1, kickSample)}
+                  sampleFunction={() => Kick(0, 1, kickSample)}
                 />
               ) : null}
               <div className="flex gap-2 w-full flex-initial">
@@ -1000,56 +749,12 @@ const InstrumentControl: React.FC<IProps> = (props: IProps) => {
         {name}
       </div>
       <div onClick={() => setShow((cur) => !cur)}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z"
-          />
-        </svg>
+        <WrenchIcon />
       </div>
       {show && (
         <div className="flex gap-4">
           <div>
-            {vol === -10 && (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M17.25 9.75L19.5 12m0 0l2.25 2.25M19.5 12l2.25-2.25M19.5 12l-2.25 2.25m-10.5-6l4.72-4.72a.75.75 0 011.28.531V19.94a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.506-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.395C2.806 8.757 3.63 8.25 4.51 8.25H6.75z"
-                />
-              </svg>
-            )}
-            {vol > -10 && (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z"
-                />
-              </svg>
-            )}
+            <SpeakerIcon mute={vol === -10} />
           </div>
           <div>
             <input
